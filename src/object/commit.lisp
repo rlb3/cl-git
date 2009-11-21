9c010c0545f1311c6a288509eacb83b3eccf17ce
Robert Boone <robo4288@gmail.com>
2009-11-21 08:31:56 -0600

    Add more attributes to commit object

--- src/object/commit.lisp
+++ src/object/commit.lisp
@@ -4,7 +4,28 @@
   ((kind
     :accessor kind
     :initarg :kind
-    :initform "Commit")))
+    :initform "Commit")
+   (tree_sha1
+    :accessor tree_sha1
+    :initarg :tree_sha1)
+   (parent_sha1
+    :accessor parent_sha1
+    :initarg :parent_sha1)
+   (author
+    :accessor author
+    :initarg :author)
+   (authored_time
+    :accessor authored_time
+    :initarg :authored_time)
+   (committer
+    :accessor committer
+    :initarg :committer)
+   (committed_time
+    :accessor committed_time
+    :initarg :committed_time
+    (comment
+     :accessor comment
+     :initarg :comment))))
 
 
 
